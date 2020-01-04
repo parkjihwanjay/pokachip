26f57efbc7fcee396845d03f6de6f0cb5d8c70b4
elksha <lksia12@naver.com>
2020-01-04 17:26:35 +0900

    hhhhhhhh

--- selenum.py
+++ selenum.py
@@ -60,40 +60,97 @@ def order(menu_list, adrs1, adrs2, store, num_list):
     try:
         driver.find_element_by_xpath(
             "//*[@id=\"search\"]/div/form/ul/li[3]/a").click()
-        driver.find_element_by_xpath("//li[contains(string(), \"%s\")]" %
-                                     b).click()
+        if(store):
+            waiting.until(EC.invisibility_of_element_located(
+                (By.XPATH, "//*[@id=\"spinner\"]")))
+
+            element11 = waiting.until(EC.element_to_be_clickable(
+                (By.XPATH, "//*[@id=\"category\"]/ul/li[1]/a")))
+            element11.click()
+            driver.find_element_by_xpath(
+                "//*[@id=\"category\"]/ul/li[15]/form/div/input").send_keys(store)
+
+            time.sleep(1)
+
+            driver.find_element_by_xpath(
+                "//*[@id=\"category_search_button\"]").click()
+
+        else:
+            waiting.until(EC.invisibility_of_element_located(
+                (By.XPATH, "//*[@id=\"spinner\"]")))
+
+            element11 = waiting.until(EC.element_to_be_clickable(
+                (By.XPATH, "//*[@id=\"category\"]/ul/li[1]/a")))
+            element11.click()
+            driver.find_element_by_xpath(
+                "//*[@id=\"category\"]/ul/li[15]/form/div/input").send_keys(lis[0])
+            driver.find_element_by_xpath(
+                "//*[@id=\"category_search_button\"]").click()
+            # waiting.until(EC.invisibility_of_element_located(
+            #     (By.XPATH, "//*[@id=\"spinner\"]")))
+
+            # element1 = waiting.until(EC.element_to_be_clickable(
+            # (By.XPATH, "//li[contains(string(), \"%s\")]" % b)))
+            # waiting.until(EC.invisibility_of_element_located(
+            # (By.XPATH, "//*[@id=\"spinner\"]")))
+            # element1.click()
 
         waiting.until(EC.invisibility_of_element_located(
             (By.XPATH, "//*[@id=\"spinner\"]")))
 
-        driver.find_element_by_xpath(
-            "//*[@id=\"content\"]/div/div[4]/div[2]/div").click()
+        element2 = waiting.until(EC.element_to_be_clickable(
+            (By.XPATH, "//*[@id=\"content\"]/div/div[4]/div[2]/div")))
+        element2.click()
 
         waiting.until(EC.invisibility_of_element_located(
             (By.XPATH, "//*[@id=\"spinner\"]")))
-
         size = len(driver.find_elements_by_xpath("//*[@id=\"menu\"]/div/div"))
 
         for i in range(3, size - 1):
             driver.find_element_by_xpath("//*[@id=\"menu\"]/div/div[%i]" %
                                          i).click()
 
-        for food in lis:
-            size2 = len(
-                driver.find_elements_by_xpath(
-                    "//li[contains(string(), \"%s\")]" % food))
+        k = 0
 
-            for i in range(0, size2):
+        for food in lis:
+            contain_list = driver.find_elements_by_xpath(
+                "//li[contains(string(), \'%s\')]" % food)
+            size2 = len(contain_list)
+
+            ind = leastword(contain_list, food)
+
+            driver.find_elements_by_xpath(
+                "//li[contains(string(), \'%s\')]" % food)[int(ind)].click()
+            for j in range(num_list[k]-1):
                 try:
-                    driver.find_elements_by_xpath(
-                        "//li[contains(string(), \"%s\")]" % food)[i].click()
-                    break
-                except ElementNotInteractableException:
-                    continue
+                    driver.find_element_by_xpath(
+                    "/html/body/div[10]/div/div[2]/div[5]/div/a[2]").click()
+                except NoSuchElementException:
+                    driver.find_element_by_xpath(
+                    "/html/body/div[10]/div/div[2]/div[4]/div/a[2]").click()
+
+            # for i in range(0, size2):
+            #     try:
+            #         driver.find_elements_by_xpath(
+            #             "//li[contains(string(), \'%s\')]" % food)[i].click()
+            #         for j in range(num_list[k]-1):
+            #             driver.find_element_by_xpath(
+            #                 "/html/body/div[10]/div/div[2]/div[5]/div/a[2]").click()
+            #         break
+            #     except ElementNotInteractableException:
+            #         continue
+
+            k += 1
 
             driver.find_element_by_class_name('btn-add-cart').click()
 
         driver.find_element_by_xpath("//a[@ng-click=\"checkout()\"]").click()
+
+        waiting.until(EC.invisibility_of_element_located(
+            (By.XPATH, "//*[@id=\"spinner\"]")))
+
+        driver.find_element_by_xpath(
+            "//*[@id=\"content\"]/div/form[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/input").send_keys(adrs2)
     except NoSuchElementException:
         if(store):
             waiting.until(EC.invisibility_of_element_located(
